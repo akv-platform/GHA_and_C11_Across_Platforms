ddd8e30121a75cdb72928d68ea1af2a80633969b
Sergey Dolin <sergey.dolin@gmail.com>
2022-05-11 21:22:12 +0500

    Update test.c

--- test.c
+++ test.c
@@ -4,12 +4,19 @@
 int main(void)
 {
 	float x = 2.25;
+	float y = 2.35;
 	
 	// Should print 2.2 on -std=c11
 	printf("2.25 (2.2) %.1f\n", x);
 	
 	// Should print 2.2 on -std=c11
 	printf("2.25 (2.2) %.1f\n", 2.25);
+		
+	// Should print 2.4 on -std=c11
+	printf("2.35 (2.4) %.1f\n", y);
+	
+	// Should print 2.2 on -std=c11
+	printf("2.35 (2.4) %.1f\n", 2.35);
 	
 	// Should print 4
 	printf("4 %d\n", sizeof(float));
@@ -19,6 +26,12 @@ int main(void)
 	
 	// Should print 3
 	printf("2.5(3) %.0f\n", round(2.5));
+		
+	// Should print 4 on -std=c11
+	printf("3.5 (4) %.0f\n", 3.5);
+	
+	// Should print 3
+	printf("3.5 (4) %.0f\n", round(3.5));
 	
 	return 0;
 }
